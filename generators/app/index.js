eab3de2402cf890257426cbe3ef7f2fcb9698107
Tyrel Witcher <tyrel.witcher@gmail.com>
2021-05-14 09:39:24 -0500

    streamlining commands when it's a craft generator
    adding prettier and lint-staged

--- generators/app/index.js
+++ generators/app/index.js
@@ -9,35 +9,41 @@ const mkdirp = require('mkdirp');
 const config = require('./config');
 
 function validURL(str) {
-  var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-  return !!pattern.test(str);
+  const pattern = new RegExp(
+    '^(https?:\\/\\/)?' + // Protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // Domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // Port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?' + // Query string
+      '(\\#[-a-z\\d_]*)?$',
+    'i'
+  ); // Fragment locator
+  return Boolean(pattern.test(str));
 }
 
-var getRandomChar = function () {
+function checkForChar(excludeCharArray, char) {
+  excludeCharArray.some((badChar) => {
+    return char === badChar;
+  });
+}
 
-  var minChar = 33; // !
-  var maxChar = 126; // ~
-  var char = String.fromCharCode(crypto.randomInt(minChar, maxChar));
-  if (["'", "\"", "\\"].some(function (badChar) { return char === badChar})) {
+const getRandomChar = function () {
+  const minChar = 33; // !
+  const maxChar = 126; // ~
+  const char = String.fromCharCode(crypto.randomInt(minChar, maxChar));
+  if (["'", '"', '\\'].some((badChar) => char === badChar)) {
     return getRandomChar();
   }
 
   return char;
-}
+};
 
 module.exports = class extends Generator {
-
   initializing() {
     this.pkg = require('../../package.json');
   }
 
   prompting() {
-
     // Branding
     this.log(`
       -----------------------------------------------------------------------------          
@@ -49,7 +55,6 @@ module.exports = class extends Generator {
                                                                                   
       -----------------------------------------------------------------------------          
     `);
-    
     // Have Yeoman greet the user.
     this.log(
       yosay(`Welcome to the finest ${chalk.red('gulp-starter-kit')} generator!`)
@@ -59,16 +64,19 @@ module.exports = class extends Generator {
       {
         type: 'input',
         name: 'projectName',
-        message: `What do you want to name this project? ${chalk.yellow('(no special characters allowed)')}`,
+        message: `What do you want to name this project? ${chalk.yellow(
+          '(no special characters allowed)'
+        )}`,
         default: this.appname,
         validate(input) {
           // Do async stuff
           if (/[~`!#$%^&*+=[\]\\';,/{}|\\":<>?]/g.test(input)) {
-              // Pass the return value in the done callback
-              return chalk.bgRed.white('No special characters allowed!');
+            // Pass the return value in the done callback
+            return chalk.bgRed.white('No special characters allowed!');
           }
-            return true;
-        }
+
+          return true;
+        },
       },
       {
         type: 'list',
@@ -76,34 +84,38 @@ module.exports = class extends Generator {
         message: 'Which type of project is this?',
         choices: [
           {
-            name: `Craft CMS ${chalk.magenta('(https://craftcms.com/docs/3.x/)')}`,
-            value: 'craft'
+            name: `Craft CMS ${chalk.magenta(
+              '(https://craftcms.com/docs/3.x/)'
+            )}`,
+            value: 'craft',
           },
           {
-            name: `Bedrock Wordpress ${chalk.magenta('(https://roots.io/bedrock/)')}`,
-            value: 'bedrock'
+            name: `Bedrock Wordpress ${chalk.magenta(
+              '(https://roots.io/bedrock/)'
+            )}`,
+            value: 'bedrock',
           },
           {
             name: 'Static HTML',
-            value: 'html'
+            value: 'html',
           },
         ],
         default: 'html',
-        store: true
+        store: true,
       },
       {
         type: 'input',
         name: 'wordpressTemplateName',
         message: 'What do you want to call your custom wordpress theme?',
-        default: answers => _.kebabCase(answers.projectName),
-        when: answers => answers.projectType.includes('bedrock')
+        default: (answers) => _.kebabCase(answers.projectName),
+        when: (answers) => answers.projectType.includes('bedrock'),
       },
       {
         type: 'input',
         name: 'craftSiteName',
         message: 'What do you want to call your Craft site?',
-        default: answers => answers.projectName,
-        when: answers => answers.projectType.includes('craft')
+        default: (answers) => answers.projectName,
+        when: (answers) => answers.projectType.includes('craft'),
       },
       {
         type: 'input',
@@ -113,13 +125,15 @@ module.exports = class extends Generator {
           return `http://${path.basename(process.cwd())}.test`;
         },
         validate(input) {
-          if(validURL(input)) {
+          if (validURL(input)) {
             return true;
-          } else {
-            return chalk.bgRed.white('Please enter a valid URL.');
           }
+
+          return chalk.bgRed.white('Please enter a valid URL.');
         },
-        when: answers => answers.projectType.includes('craft') || answers.projectType.includes('bedrock')
+        when: (answers) =>
+          answers.projectType.includes('craft') ||
+          answers.projectType.includes('bedrock'),
       },
       {
         type: 'input',
@@ -129,25 +143,26 @@ module.exports = class extends Generator {
           if (input === '') {
             return chalk.bgRed.white('Database name cannot be blank.');
           }
+
           return true;
         },
-        when: answers => answers.projectType.includes('craft') || answers.projectType.includes('bedrock')
+        when: (answers) => answers.projectType.includes('bedrock'),
       },
       {
         type: 'input',
         name: 'dbUser',
         message: 'What is the username for the database?',
         default: 'root',
-        when: answers => answers.projectType.includes('craft') || answers.projectType.includes('bedrock')
+        when: (answers) => answers.projectType.includes('bedrock'),
       },
       {
         type: 'password',
         name: 'dbPassword',
-        message(answers) { 
+        message(answers) {
           return `What is the password for ${answers.dbUser}?`;
         },
         default: '',
-        when: answers => answers.projectType.includes('craft') || answers.projectType.includes('bedrock')
+        when: (answers) => answers.projectType.includes('bedrock'),
       },
       {
         type: 'checkbox',
@@ -155,62 +170,68 @@ module.exports = class extends Generator {
         message: 'Which additional features would you like to include?',
         choices: [
           {
-            name: `Vanilla Lazyload Javascript ${chalk.magenta('(https://github.com/verlok/vanilla-lazyload)')}`,
+            name: `Vanilla Lazyload Javascript ${chalk.magenta(
+              '(https://github.com/verlok/vanilla-lazyload)'
+            )}`,
             value: 'includeLazyload',
-            checked: false
+            checked: false,
           },
           {
             name: `Bootstrap - ${chalk.magenta('v4.4.0')}`,
             value: 'includeBootstrap',
-            checked: false
+            checked: false,
           },
           {
-            name: `Alpine.js ${chalk.magenta('(https://github.com/alpinejs/alpine)')}`,
+            name: `Alpine.js ${chalk.magenta(
+              '(https://github.com/alpinejs/alpine)'
+            )}`,
             value: 'includeAlpine',
-            checked: true
+            checked: true,
           },
           {
-            name: `Tailwind CSS - ${chalk.magenta('v2.0.1 (https://github.com/tailwindlabs/tailwindcss/tree/v2.0.1)')}`,
+            name: `Tailwind CSS - ${chalk.magenta(
+              'v2.0.1 (https://github.com/tailwindlabs/tailwindcss/tree/v2.0.1)'
+            )}`,
             value: 'includeTailwind',
-            checked: true
+            checked: true,
           },
         ],
-        store: true
+        store: true,
       },
       {
         type: 'confirm',
         name: 'includeJQuery',
         message: 'Would you like to include jQuery(v 3.4)?',
         default: false,
-        when: answers => !answers.features.includes('includeBootstrap')
-      }
-    ]).then(answers => {
-        this.projectType = answers.projectType;
-        this.wordpressTemplateName = answers.wordpressTemplateName;
-        this.craftSiteName = answers.craftSiteName;
-        this.siteUrl = answers.siteUrl;
-        this.dbName = answers.dbName;
-        this.dbUser = answers.dbUser;
-        this.dbPassword = answers.dbPassword;
+        when: (answers) => !answers.features.includes('includeBootstrap'),
+      },
+    ]).then((answers) => {
+      this.projectType = answers.projectType;
+      this.wordpressTemplateName = answers.wordpressTemplateName;
+      this.craftSiteName = answers.craftSiteName;
+      this.siteUrl = answers.siteUrl;
+      this.dbName = answers.dbName;
+      this.dbUser = answers.dbUser;
+      this.dbPassword = answers.dbPassword;
 
-        const features = answers.features;
-        const hasFeature = feat => features && features.includes(feat);
+      const { features } = answers;
+      const hasFeature = (feat) => features && features.includes(feat);
 
-        // manually deal with the response, get back and store the results.
-        // we change a bit this way of doing to automatically do this in the self.prompt() method.
-        this.includeBootstrap = hasFeature('includeBootstrap');
-        this.includeTailwind = hasFeature('includeTailwind');
-        this.includeAlpine = hasFeature('includeAlpine');
-        this.includeLazyload = hasFeature('includeLazyload');
-        this.includeJQuery = answers.includeJQuery;
-        this.projectName = answers.projectName;
-      });
-    }
+      // Manually deal with the response, get back and store the results.
+      // we change a bit this way of doing to automatically do this in the self.prompt() method.
+      this.includeBootstrap = hasFeature('includeBootstrap');
+      this.includeTailwind = hasFeature('includeTailwind');
+      this.includeAlpine = hasFeature('includeAlpine');
+      this.includeLazyload = hasFeature('includeLazyload');
+      this.includeJQuery = answers.includeJQuery;
+      this.projectName = answers.projectName;
+    });
+  }
 
   writing() {
     const pkgJson = {
       dependencies: {},
-      devDependencies: {}
+      devDependencies: {},
     };
 
     const templateData = {
@@ -234,8 +255,12 @@ module.exports = class extends Generator {
       includeAlpine: this.includeAlpine,
       includeLazyload: this.includeLazyload,
       generateSalt: () => {
-        return Array.apply(null, Array(64)).map(getRandomChar).join("");
-      }
+        const saltLength = 64;
+
+        return Array(...Array(saltLength))
+          .map(getRandomChar)
+          .join('');
+      },
     };
 
     const copy = (input, output) => {
@@ -251,49 +276,45 @@ module.exports = class extends Generator {
     };
 
     // Render Files
-    config.filesToRender.forEach(file => {
+    config.filesToRender.forEach((file) => {
       copyTpl(file.input, file.output, templateData);
     });
 
     // Copy Files
-    config.filesToCopy.forEach(file => {
+    config.filesToCopy.forEach((file) => {
       copy(file.input, file.output);
     });
 
     // Create extra directories
-    config.dirsToCreate.forEach(item => {
+    config.dirsToCreate.forEach((item) => {
       mkdirp(item);
     });
 
-    if(this.projectType === 'bedrock' || this.projectType === 'craft') {
-
-      config[this.projectType].dirsToCreate(this).forEach(item => {
+    if (this.projectType === 'bedrock' || this.projectType === 'craft') {
+      config[this.projectType].dirsToCreate(this).forEach((item) => {
         mkdirp(item);
       });
-      
-      config[this.projectType].filesToCopy.forEach(file => {
+
+      config[this.projectType].filesToCopy.forEach((file) => {
         copy(file.input, file.output);
       });
 
-      config[this.projectType].filesToRender(this).forEach(file => {
+      config[this.projectType].filesToRender(this).forEach((file) => {
         copyTpl(file.input, file.output, templateData);
       });
-
     } else if (this.projectType === 'html') {
-
       copyTpl('_index.html.ejs', 'src/index.html', templateData);
-
     }
 
     if (this.includeBootstrap) {
       pkgJson.dependencies = {
         bootstrap: '^4.4.0',
         'popper.js': '^1.15.0',
-        jquery: '^3.4.1'
-      }
+        jquery: '^3.4.1',
+      };
     }
-    
-    if (this.includeJQuery && !pkgJson.dependencies.hasOwnProperty('jquery')) {
+
+    if (this.includeJQuery && !_.has(pkgJson.dependencies, 'jquery')) {
       pkgJson.dependencies.jquery = '^3.4.1';
     }
 
@@ -301,7 +322,7 @@ module.exports = class extends Generator {
       pkgJson.dependencies.alpinejs = '^2.7.1';
     }
 
-    if(this.includeLazyload) {
+    if (this.includeLazyload) {
       pkgJson.dependencies['vanilla-lazyload'] = '^17.1.3';
     }
 
@@ -313,52 +334,45 @@ module.exports = class extends Generator {
     }
 
     this.fs.extendJSON(this.destinationPath('package.json'), pkgJson);
-  
   }
 
   install() {
-
     this.npmInstall();
 
     if (this.projectType === 'bedrock' || this.projectType === 'craft') {
-
       this.spawnCommandSync('composer', ['install']);
-
     }
-
   }
 
   end() {
-
-    if(this.projectType === 'bedrock' || this.projectType === 'craft') {
-
-      this.log(yosay('allo!\nJust gonna build the frontend files for the first time since they\'re in the .gitignore.'))
+    if (this.projectType === 'bedrock' || this.projectType === 'craft') {
+      this.log(
+        yosay('allo!\nJust gonna build the frontend files for the first time.')
+      );
 
       this.spawnCommandSync('npm', ['run', 'build:dev']);
-
     }
 
-    if(this.projectType === 'html') {
-
-      this.log(yosay(`Thanks for using Gulp Starter Kit!\nTry running 'npm run serve' to check out your new fancy project!\nCheers 🍻!`));
-
+    if (this.projectType === 'html') {
+      this.log(
+        yosay(
+          `Thanks for using Gulp Starter Kit!\nTry running 'npm run serve' to check out your new fancy project!\nCheers 🍻!`
+        )
+      );
     } else {
-
       this.log(yosay(`Thanks for using Gulp Starter Kit!\nCheers 🍻!`));
-
     }
 
     if (this.projectType === 'craft') {
-
       // Generate security key and app id in .env file
-      this.spawnCommandSync('php', ['craft', 'setup/security-key']);
-      this.spawnCommandSync('php', ['craft', 'setup/app-id']);
+      // this.spawnCommandSync('php', ['craft', 'setup/security-key']);
+      // this.spawnCommandSync('php', ['craft', 'setup/app-id']);
 
-      this.log(yosay('g\'day!\nGonna run you through `php craft install` process now.'))
+      this.log(
+        yosay("g'day!\nGonna run you through `php craft install` process now.")
+      );
 
-      this.spawnCommandSync('php', ['craft', 'install']);
-
+      this.spawnCommandSync('php', ['craft', 'setup']);
     }
-
   }
 };
